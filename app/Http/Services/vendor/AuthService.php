c2e352dc97bd1a10c179ec080ed257ed2faf6771
Moutaz Takahje <mhdmoutaztakahje@gmail.com>
2025-11-03 00:32:29 +0300

    Edit vendor register

--- app/Http/Services/vendor/AuthService.php
+++ app/Http/Services/vendor/AuthService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services\vendor;
 
 use App\Models\User;
 use App\Enums\RoleUserEnum;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class AuthService
@@ -17,22 +18,29 @@ class AuthService
     }
     public function registerVendor(array $data)
     {
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'phone_number' => $data['phone_number'],
-            'role' => RoleUserEnum::Vendor,
-        ]);
-        $vendor = $user->vendor()->create([
-            'national_id' => $data['national_id'],
-            'business_type' => $data['business_type'],
-            'description' => $data['description'],
-            'has_store' => $data['has_store'],
-            'pickup_address' => $data['pickup_address'],
-            'pickup_hours' => $data['pickup_hours'],
-        ]);
-        return $vendor;
+        DB::beginTransaction();
+        try {
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => $data['password'],
+                'phone_number' => $data['phone_number'],
+                'role' => RoleUserEnum::Vendor,
+            ]);
+            $vendor = $user->vendor()->create([
+                'national_id' => $data['national_id'],
+                'business_type' => $data['business_type'],
+                'description' => $data['description'],
+                'has_store' => $data['has_store'],
+                'pickup_address' => $data['pickup_address'],
+                'pickup_hours' => $data['pickup_hours'],
+            ]);
+            DB::commit();
+            return $vendor;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw new \Exception('Registration failed' );
+        }
     }
     public function loginVendor(array $data)
     {
